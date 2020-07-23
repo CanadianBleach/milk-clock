63864ee2e32244c8dc35a96934baaa7c529cb28e
CanadianBleach <45185643+CanadianBleach@users.noreply.github.com>
2020-07-23 18:55:54 -0400

    Updates

---
+++ precache-manifest.2b99623352abd0946ec1b16d34840de9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2722cdedb090e5634d9a49b0fb00e964",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09797b78f835e3fcfd24",
+    "url": "/static/css/main.7899d216.chunk.css"
+  },
+  {
+    "revision": "f6709ea372f79082d62f",
+    "url": "/static/js/2.e70ec385.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e70ec385.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09797b78f835e3fcfd24",
+    "url": "/static/js/main.5f88dbac.chunk.js"
+  },
+  {
+    "revision": "d4d8e34d6fb6ba20ec31",
+    "url": "/static/js/runtime-main.4cdc153b.js"
+  },
+  {
+    "revision": "115e2f4ca7de8c851bebb533086a8d6b",
+    "url": "/static/media/milk_base.115e2f4c.svg"
+  }
+]);
